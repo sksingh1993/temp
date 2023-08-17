2a4310eee065c4ad1e5537f090226ac934d7a6e2
Suraj Singh <kumarsurajsingh93@gmail.com>
2023-08-17 15:55:37 +0530

    initial commit

---
+++ src/main/java/com/tech/soft/cicd/CiCdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tech.soft.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech/soft/cicd/CiCdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.soft.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
